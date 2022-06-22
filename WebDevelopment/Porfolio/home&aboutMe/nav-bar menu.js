212fb566a334cf3bb1dd86d1d613ba39ab99f709
hhjkjkjk <94072596+hhjkjkjk@users.noreply.github.com>
2022-06-22 17:50:50 +0700

    Delete WebDevelopment/Porfolio directory

--- WebDevelopment/Porfolio/home&aboutMe/nav-bar menu.js
+++
@@ -1,6 +0,0 @@
-const toggleButton = document.getElementsByClassName('toggle-button')[0]
-const navbarMenu = document.getElementsByClassName('menu')[0]
-
-toggleButton.addEventListener('click', () => {
-    navbarMenu.classList.toggle('active')
-})
